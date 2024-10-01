f6c5849cf8090cf837e36effae7db51ea9c6b156
BabuBishal <bohorabishal20@gmail.com>
2024-10-01 15:41:56 +0545

    contact page

--- app/(root)/about/page.jsx
+++ app/(root)/about/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
     <div className="w-full mx-5 my-5 lg:w-[60%] lg:mx-auto">
       <div className="relative h-[500px] w-full ">
         <Image
-        src="/about.jpg"
+        src="/assets/about.jpg"
         fill
         alt="about image"
         objectFit="cover"
@@ -14,7 +14,7 @@ const page = () => {
         <Overlay />
         <h1 className="flex absolute w-full h-full justify-center items-center text-4xl font-extrabold uppercase text-white">About us</h1>
       </div>
-      <div className="leading-8 text-lg bg-white relative mt-[-20px] rounded-lg p-5 shadow-xl max-md:mt-0 max-md:w-full max-md:bg-transparent max-md:shadow-none text-center ">
+      <div className="leading-8 text-lg bg-white relative mt-[-70px] rounded-lg p-5 shadow-xl w-[90%] mx-auto max-md:mt-0 max-md:w-full max-md:bg-transparent max-md:shadow-none text-center ">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero incidunt impedit eveniet, delectus reprehenderit totam, omnis iste, fugiat tenetur similique vel doloribus reiciendis aliquam. Tenetur dolores sint perferendis natus nisi!
         Quos atque ab optio eveniet adipisci, ea eaque porro molestias sequi hic, ad quam voluptatum vel odio deleniti delectus repellat fugiat, incidunt officia! Blanditiis quas, a laborum possimus natus magni?
         Suscipit, quis explicabo minus repellat adipisci possimus, ullam inventore natus voluptatibus qui voluptatum recusandae deserunt veniam aut asperiores! Nobis nesciunt iure corrupti sit maiores, laboriosam ullam veniam architecto. Necessitatibus, iure?
@@ -23,7 +23,7 @@ const page = () => {
         </p>
         <div className="w-full flex items-center justify-center" >
 <Image
-src="/signature.png"
+src="/assets/signature.png"
 width={500}
 height={500}
 alt="signature"
